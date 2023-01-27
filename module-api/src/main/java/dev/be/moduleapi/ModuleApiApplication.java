e72c7ef03fb93fcd1c32eb0e10645492704c0987
gdtknight <gdtknight@naver.com>
2023-01-27 20:04:27 +0900

    ModuleApiApplication 코드 포맷 수정

--- module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ModuleApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ModuleApiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ModuleApiApplication.class, args);
+  }
 }
