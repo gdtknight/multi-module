600bf970355a68dd4595b80fa153a3e3e58caa29
gdtknight <gdtknight@naver.com>
2023-01-28 10:54:19 +0900

    #12 - modify DemoService
    DemoService 내에서 MemberDomain, MemberRepository 테스트 부분을 추가한다.

--- module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,25 +1,38 @@
 package dev.be.moduleapi.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dev.be.moduleapi.exception.CustomException;
+import dev.be.modulecommon.domain.Member;
 import dev.be.modulecommon.enums.CodeEnum;
+import dev.be.modulecommon.repository.MemberRepository;
 import dev.be.modulecommon.service.CommonDemoService;
-import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor
 public class DemoService {
+  private static final Logger logger = LoggerFactory.getLogger(DemoService.class);
 
   private final CommonDemoService commonDemoService;
+  private final MemberRepository memberRepository;
+
+  DemoService(@Autowired CommonDemoService commonDemoService, @Autowired MemberRepository memberRepository) {
+    this.commonDemoService = commonDemoService;
+    this.memberRepository = memberRepository;
+  }
 
   public String save() {
-    System.out.println(CodeEnum.SUCCESS.getCode());
-    System.out.println(commonDemoService.commonService());
+    memberRepository.save(Member.builder()
+        .name(Thread.currentThread().getName())
+        .build());
     return "save";
   }
 
   public String find() {
+    int size = memberRepository.findAll().size();
+    logger.info("DB Size : " + size);
     return "find";
   }
 
